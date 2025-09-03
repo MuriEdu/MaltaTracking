2e05cf12448f92bece2fdc064528d1102ad8587a
Murilo Ramos <muriloeduardo6@gmail.com>
2025-09-02 22:38:54 -0300

    update new transaction

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -15,10 +15,15 @@ const TransactionForm = ({ onAddTransaction }) => {
       return;
     }
 
+    console.log('Amount before parsing:', amount);
+    console.log('Type:', type);
+
     const newTransaction = {
       text,
       amount: type === 'expense' ? -parseFloat(amount) : parseFloat(amount),
     };
+
+    console.log('New Transaction:', newTransaction);
 
     onAddTransaction(newTransaction);
 
@@ -27,36 +32,34 @@ const TransactionForm = ({ onAddTransaction }) => {
   };
 
   return (
-    <div className="card">
-      <div className="card-body">
-        <h3 className="card-title text-center">Add new transaction</h3>
-        <form onSubmit={onSubmit}>
-          <div className="btn-group">
-            <button type="button" className={`btn btn-income ${type === 'income' ? 'active' : ''}`} onClick={() => setType('income')}>Income</button>
-            <button type="button" className={`btn btn-expense ${type === 'expense' ? 'active' : ''}`} onClick={() => setType('expense')}>Expense</button>
-          </div>
-          <div className="form-group">
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)} className="form-control" id="text" placeholder="Description" />
-          </div>
-          <div className="form-group">
-            <CurrencyInput
-              id="amount"
-              name="amount"
-              placeholder="Amount"
-              defaultValue={0}
-              decimalsLimit={2}
-              onValueChange={(value, name) => setAmount(value)}
-              prefix="R$ "
-              groupSeparator="."
-              decimalSeparator=","
-              className="form-control"
-              inputMode="decimal"
-            />
-          </div>
-          <button className="btn btn-primary btn-block mt-3">Add transaction</button>
-        </form>
-      </div>
-    </div>
+    <>
+      <h3 className="card-title">Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="btn-group">
+          <button type="button" className={`btn btn-income ${type === 'income' ? 'active' : ''}`} onClick={() => setType('income')}>Income</button>
+          <button type="button" className={`btn btn-expense ${type === 'expense' ? 'active' : ''}`} onClick={() => setType('expense')}>Expense</button>
+        </div>
+        <div className="form-group">
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} className="form-control" id="text" placeholder="Description" />
+        </div>
+        <div className="form-group">
+          <CurrencyInput
+            id="amount"
+            name="amount"
+            placeholder="Amount"
+            defaultValue={0}
+            decimalsLimit={2}
+            onValueChange={(value, name) => setAmount(value)}
+            prefix="R$ "
+            groupSeparator="."
+            decimalSeparator=","
+            className="form-control"
+            inputMode="decimal"
+          />
+        </div>
+        <button className="btn btn-primary mt-3">Add transaction</button>
+      </form>
+    </>
   );
 };
 
